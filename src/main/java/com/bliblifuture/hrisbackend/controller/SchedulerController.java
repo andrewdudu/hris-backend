edc6ebb63d8c9d9f9038c34585bbd8c0a6ba3e61
keiyaW <waridtheeleven@gmail.com>
2021-01-10 16:53:13 +0700

    scheduler api update leave

--- src/main/java/com/bliblifuture/hrisbackend/controller/SchedulerController.java
+++ src/main/java/com/bliblifuture/hrisbackend/controller/SchedulerController.java
@@ -4,7 +4,7 @@ import com.blibli.oss.command.CommandExecutor;
 import com.blibli.oss.common.response.Response;
 import com.blibli.oss.common.response.ResponseHelper;
 import com.bliblifuture.hrisbackend.command.AutoClockoutCommand;
-import com.bliblifuture.hrisbackend.command.UpdateAnnualLeaveCommand;
+import com.bliblifuture.hrisbackend.command.UpdateLeaveQuota;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +22,7 @@ public class SchedulerController {
 
     @PostMapping("/annual-leave")
     public Mono<Response<String>> updateAnnualLeave(){
-        return commandExecutor.execute(UpdateAnnualLeaveCommand.class, "")
+        return commandExecutor.execute(UpdateLeaveQuota.class, "")
                 .map(ResponseHelper::ok)
                 .subscribeOn(Schedulers.elastic());
     }
